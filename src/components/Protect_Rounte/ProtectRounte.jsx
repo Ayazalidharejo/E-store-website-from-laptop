00b793f774d319cf0b0fa162428876f71f1c96e4
Ayazalidharejo <ayazali.xwave@gmail.com>
2025-01-28 11:37:05 +0500

    signin changing

--- src/components/Protect_Rounte/ProtectRounte.jsx
+++ src/components/Protect_Rounte/ProtectRounte.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
+import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
 
 const ProtectRounte = ({children}) => {
   const [token ,settoken]=useState("")
@@ -12,10 +13,19 @@ const ProtectRounte = ({children}) => {
   if (token) {
     return <>{children} </>
   }else{
-return<div className='d-flex align-items-center justify-content-center mt-5'>
-<h1>you don't have accese  </h1>
-<Link to="sign-in"> <button className='border-0 bg-success text-white'>  Signin First </button> </Link>
-</div>
+return<div className='d-flex justify-content-center align-items-center vh-100'>
+      <div className='text-center'>
+        <h1 className="mb-4">You must sign in to access This Page</h1>
+        <div className='d-flex align-items-center justify-content-center'>
+          <ArrowRightAltIcon className='fs-1' />
+          <Link to="sign-in">
+            <button className='border-0 bg-success text-white p-3 fs-4 rounded ms-3'>
+              Sign in First
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
   }
  
 }
